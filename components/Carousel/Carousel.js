8dc8c3d43ec7e897c395fee87579a787dfe14a5f
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-06-15 12:00:59 -0700

    started stretch, didn't finish toggle methods

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,28 @@
 class Carousel {
+    constructor(carousel) {
+        this.carousel = carousel;
+        this.images = this.carousel.querySelectorAll("img")
+        this.rButton = this.carousel.querySelector(".right-button");
+        this.lButton = this.carousel.querySelector(".left-button");
+        this.currentIndex = 0;
+        currentImage = this.images[this.currentIndex].style.display = "block";
+        
+        this.rButton.addEventListener("click", () => this.toggleRight());
+        this.lButton.addEventListener("click", () => this.toggleLeft());
+    }
 
+    toggleRight() {
+
+    }
+
+    toggleLeft() {
+
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelectorAll(".carousel");
+
+carousel.forEach(carousel => new Carousel(carousel));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
